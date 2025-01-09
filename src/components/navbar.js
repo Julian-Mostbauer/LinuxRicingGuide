bd577484bbba934cf5f0d97a14b1d2354ada386f
Arthur <173360990+adirnberger-htl@users.noreply.github.com>
2025-01-09 10:49:35 +0100

    fixing the text titel card in index.html

---
+++ src/components/navbar.js
@@ -0,0 +1,97 @@
+const navbarHTML = `
+<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.html">${document.title}</a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+            class="collapse navbar-collapse"
+            id="navbarSupportedContent"
+        >
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="intro.html"
+                        >Introduction</a
+                    >
+                </li>
+                <li class="nav-item dropdown">
+                    <a
+                        class="nav-link dropdown-toggle"
+                        href="#"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                    >
+                        Links
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a
+                                class="dropdown-item"
+                                href="intro.html"
+                                >Introduction</a
+                            >
+                        </li>
+                        <li>
+                            <a
+                                class="dropdown-item"
+                                href="distros.html"
+                                >Distributions</a
+                            >
+                        </li>
+                        <li>
+                            <a
+                                class="dropdown-item"
+                                href="package-managers.html"
+                                >Package Managers</a
+                            >
+                        </li>
+                        <li>
+                            <a
+                                class="dropdown-item"
+                                href="personal-setups.html"
+                                >Setups</a
+                            >
+                        </li>
+
+                        <li>
+                            <a
+                                class="dropdown-item"
+                                href="window-managers.html"
+                                >Window Managers</a
+                            >
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input
+                    class="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                />
+                <button class="btn btn-primary" type="submit">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </form>
+        </div>
+    </div>
+</nav>`
+
+// Dummy DOM element to hold navbar
+const template = document.createElement('template')
+template.innerHTML = navbarHTML.trim()
+
+document
+    .getElementById('navbar-component')
+    .replaceWith(template.content.firstChild)
