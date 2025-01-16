0003439ebbc38166eaaa6da511a468ec48aa887d
Julian Mostbauer <julian.mostbauer@outlook.com>
2025-01-16 09:44:28 +0100

    removed soon unecessary cards changed styling on index

--- src/components/card-big-image.js
+++ src/components/card-big-image.js
@@ -1,25 +1,30 @@
 import ComponentBuilder from './component-builder.js'
 
+const cardStyle = `
+box-shadow: 0px 0px 10px 5px rgba(0, 0, 0, 1);
+max-width: 25rem;
+`
+
 const cardCode = `
 <div class="col-12 mt-4">
-                    <div class="card w-100">
-                        <img
-                            src="{{image-path}}"
-                            class="card-img-top"
-                            alt="..."
-                            style="object-fit: cover; height: 15rem"
-                        />
-                        <div class="card-body text-start">
-                            <h5 class="card-title">{{title}}</h5>
-                            <p class="card-text">
-                                {{description}}
-                            </p>
-                            <a href="{{link}}" class="btn btn-primary"
-                                ><i class="fa-solid fa-link"></i> Visit</a
-                            >
-                        </div>
-                    </div>
-                </div>
+    <div class="card w-100" style="${cardStyle}">
+        <img
+            src="{{image-path}}"
+            class="card-img-top"
+            alt="..."
+            style="object-fit: cover; height: 15rem"
+        />
+        <div class="card-body text-start">
+            <h5 class="card-title">{{title}}</h5>
+            <p class="card-text">
+                {{description}}
+            </p>
+            <a href="{{link}}" class="btn btn-primary"
+                ><i class="fa-solid fa-link"></i> Visit</a
+            >
+        </div>
+    </div>
+</div>
 `
 
 const cardBuilder = new ComponentBuilder(cardCode, 'card-big-image')
