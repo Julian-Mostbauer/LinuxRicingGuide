df324920ec3579d96bfe76c3a95d35a4c892fb7f
Julian Mostbauer <julian.mostbauer@outlook.com>
2025-01-17 22:09:35 +0100

    fixed card height inconsistency

--- src/components/card-big-image.js
+++ src/components/card-big-image.js
@@ -1,19 +1,23 @@
 import ComponentBuilder from './component-builder.js'
 
 const cardCode = `
-<div class="col-12 mt-4">
+<div class="col-12 mt-4" style="height: calc(100% - 1.5rem);">
     <div class="card big-img">
-        <img
-            src="{{image-path}}"
-            class="card-img-top"
-            alt="..."
-            style="object-fit: cover; height: 15rem"
-        />
+        <div style="flex: 1; display: flex; align-items: center;">
+            <img
+                src="{{image-path}}"
+                class="card-img-top"
+                alt="..."
+                style="object-fit: cover; height: 15rem;"
+            />
+        </div>
         <div class="card-body text-start">
             <h5 class="card-title">{{title}}</h5>
             <p class="card-text">
                 {{description}}
             </p>
+        </div>
+        <div class="card-footer" style="text-align: center;">
             <a href="{{link}}" class="btn btn-primary" style="min-width: 100%"
                 ><i class="fa-solid fa-link"></i> Visit</a
             >
--- src/components/card-small-image.js
+++ src/components/card-small-image.js
@@ -2,12 +2,12 @@ import ComponentBuilder from './component-builder.js'
 
 const cardCode = `
 <div class="col-12 mt-4">
-    <div class="card small-img w-100 d-flex flex-row align-items-stretch">
+    <div class="card small-img w-100 d-flex flex-column flex-md-row align-items-stretch">
         <img
         src="{{image-path}}"
-        class="card-img-left"
+        class="card-img-top card-img-md-left"
         alt="..."
-        style="object-fit: cover; width: 15rem; height: 15rem; border-right: 1px solid #424549; padding: 1rem;"
+        style="object-fit: cover; width: 100%; height: auto; border-bottom: 1px solid #424549; padding: 1rem;"
         />
         <div class="card-body text-start d-flex flex-column justify-content-between">
             <div>
