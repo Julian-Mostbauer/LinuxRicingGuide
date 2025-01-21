98faba287f6583589011b0a45e55ddf070098098
Integr-0 <erik.reitbauer@outlook.de>
2025-01-21 09:05:08 +0100

    Changed navbar to a better design

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -20,19 +20,55 @@ const navbarCode = `
             id="navbarSupportedContent"
         >
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="window-managers.html"
-                        >Window Managers</a
-                    >
+                <li class="nav-item dropdown">
+                    <a
+                        class="nav-link dropdown-toggle"
+                        href="#"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                    >Desktop</a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a
+                                class="dropdown-item"
+                                href="window-managers.html"
+                            >Window Managers</a>
+                        </li>
+                        <li>
+                            <a
+                                class="dropdown-item"
+                                href="distros.html"
+                            >Distributions</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a
+                        class="nav-link dropdown-toggle"
+                        href="#"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                    >Terminal</a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a
+                                class="dropdown-item"
+                                href="terminals.html"
+                            >Terminals</a>
+                        </li>
+                        <li>
+                            <a
+                                class="dropdown-item"
+                                href="terminal-themes.html"
+                            >Terminal Themes</a>
+                        </li>
+                    </ul>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" aria-current="page" href="package-managers.html"
                         >Package Managers</a
-                    >
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="distros.html"
-                        >Distributions</a
                     >
                 </li>
                 <li class="nav-item">
