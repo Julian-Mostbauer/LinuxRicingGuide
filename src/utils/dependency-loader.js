874f8b3527a480b3d9da65c2d8e6d3a2bd1a719b
Julian Mostbauer <julian.mostbauer@outlook.com>
2025-01-24 21:54:56 +0100

    local bootstrap install with npm loader with just needed files

--- src/utils/dependency-loader.js
+++ src/utils/dependency-loader.js
@@ -11,25 +11,33 @@
         />
 */
 
+const bootstrapFiles = [
+   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/js/dist/base-component.js',
+   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/js/dist/modal.js',
+   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/js/dist/dropdown.js',
+   'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js',
+]
+
 const loadStylesAndScripts = () => {
   const fontAwesomeScript = document.createElement('script');
   fontAwesomeScript.src = "https://kit.fontawesome.com/0a7e2ccef9.js";
   fontAwesomeScript.crossOrigin = "anonymous";
+  document.head.appendChild(fontAwesomeScript);
   
   const bootstrapLink = document.createElement('link');
   bootstrapLink.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css";
   bootstrapLink.rel = "stylesheet";
   bootstrapLink.integrity = "sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH";
   bootstrapLink.crossOrigin = "anonymous";
-
-  const bootstrapScript = document.createElement('script');
-  bootstrapScript.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js";
-  bootstrapScript.integrity = "sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz";
-  bootstrapScript.crossOrigin = "anonymous";
-
-  document.head.appendChild(fontAwesomeScript);
   document.head.appendChild(bootstrapLink);
-  document.head.appendChild(bootstrapScript);
+
+  for (const file of bootstrapFiles) {
+    const script = document.createElement('script');
+    script.src = file;
+//    script.integrity = "sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz";
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);    
+  }
 };
 
 loadStylesAndScripts();
