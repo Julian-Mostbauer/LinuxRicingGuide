5954866c3e0f47da02c6703da4171103dec3d308
Julian Mostbauer <julian.mostbauer@outlook.com>
2025-01-24 22:08:02 +0100

    added img srcset based on lighthouse report

--- src/components/card-big-image.js
+++ src/components/card-big-image.js
@@ -3,14 +3,17 @@ import ComponentBuilder from './component-builder.js'
 const cardCode = `
 <div class="col-12 mt-4" style="height: calc(100% - 1.5rem);">
     <div class="card big-img">
-        <div style="flex: 1; display: flex; align-items: center;">
-            <img
-                src="{{image-path}}"
-                class="card-img-top"
-                alt="..."
-                style="object-fit: cover; height: 15rem;"
-            />
-        </div>
+    <img
+    src="{{image-path}}"
+    class="card-img-top"
+    alt="..."
+    loading="lazy"
+    style="object-fit: cover; height: 15rem;"
+    srcset="{{image-path}}?w=400 400w,
+            {{image-path}}?w=800 800w,
+            {{image-path}}?w=1200 1200w"
+    sizes="(max-width: 600px) 400px, (max-width: 1024px) 800px, 1200px"
+/>
         <div class="card-body text-start">
             <h5 class="card-title">{{title}}</h5>
             <p class="card-text">
