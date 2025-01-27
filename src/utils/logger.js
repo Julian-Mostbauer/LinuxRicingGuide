8054392e55667d47c4645d18d50879f6bc073dee
Julian Mostbauer <julian.mostbauer@outlook.com>
2025-01-27 16:58:16 +0100

    reworked logger system

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,4 +1,5 @@
 const PRIORITY = {
+    DISABLED: 0,
     DEBUG: 1,
     INFO: 2,
     WARN: 3,
@@ -6,59 +7,28 @@ const PRIORITY = {
 }
 
 class Logger {
-    constructor() {
-        if (Logger.instance) {
-            return Logger.instance
-        }
-
-        this.enabled = true
-        this.priorityLevel = PRIORITY.DEBUG
-        Logger.instance = this
-    }
-
-    setPriority(level) {
-        this.priorityLevel = level
-    }
-
-    enable() {
-        this.enabled = true
-    }
-
-    disable() {
-        this.enabled = false
+    constructor(enabled = true, priorityLevel = PRIORITY.INFO) {
+        this.enabled = enabled
+        this.priorityLevel = priorityLevel
     }
 
     log(message, priority = PRIORITY.INFO) {
-        if (this.enabled && priority >= this.priorityLevel) {
-            console.log(
-                `[LOG] ${new Date().toISOString()} [${this.getPriorityString(
-                    priority
-                )}]: ${message}`
-            )
-        }
+        if (priority < this.priorityLevel) return
+        console.log(
+            `[LOG] ${new Date().toISOString()} [${this.getPriorityString(
+                priority
+            )}]: ${message}`
+        )
     }
 
     table(obj, priority = PRIORITY.INFO) {
-        if (this.enabled && priority >= this.priorityLevel) {
-            console.log(
-                `[LOG] ${new Date().toISOString()} [${this.getPriorityString(
-                    priority
-                )}]:`
-            )
-            console.table(obj)
-        }
-    }
-
-    time(label) {
-        if (this.enabled) {
-            console.time(label)
-        }
-    }
-
-    timeEnd(label) {
-        if (this.enabled) {
-            console.timeEnd(label)
-        }
+        if (priority < this.priorityLevel) return
+        console.log(
+            `[LOG] ${new Date().toISOString()} [${this.getPriorityString(
+                priority
+            )}]:`
+        )
+        console.table(obj)
     }
 
     getPriorityString(priority) {
@@ -66,9 +36,9 @@ class Logger {
     }
 }
 
-const logger = new Logger()
-logger.setPriority(PRIORITY.DEBUG)
+const logger = new Logger(PRIORITY.DISABLED)
 
-Object.freeze(logger) // Optional: to prevent modifications to the singleton instance
+// immutable global instance
+Object.freeze(logger)
 
 export { logger, PRIORITY }
