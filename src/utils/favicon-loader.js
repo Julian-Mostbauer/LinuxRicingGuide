3da75cbe0e6ace10932bd82b63ebcec0a5810bc5
Julian Mostbauer <julian.mostbauer@outlook.com>
2025-01-28 09:10:49 +0100

    fixed missing android chrome icons

--- src/utils/favicon-loader.js
+++ src/utils/favicon-loader.js
@@ -19,6 +19,20 @@ document.addEventListener('DOMContentLoaded', function () {
     linkIcon16.href = '/assets/favicon_io/favicon-16x16.png'
     document.head.appendChild(linkIcon16)
 
+    const linkIconChrome192 = document.createElement('link')
+    linkIcon16.rel = 'icon'
+    linkIcon16.type = 'image/png'
+    linkIcon16.sizes = '192x192'
+    linkIcon16.href = '/assets/favicon_io/android-chrome-192x192.png'
+    document.head.appendChild(linkIconChrome192)
+
+    const linkIconChrome512 = document.createElement('link')
+    linkIcon16.rel = 'icon'
+    linkIcon16.type = 'image/png'
+    linkIcon16.sizes = '512x512'
+    linkIcon16.href = '/assets/favicon_io/android-chrome-512x512.png'
+    document.head.appendChild(linkIconChrome512)
+
     const linkManifest = document.createElement('link')
     linkManifest.rel = 'manifest'
     linkManifest.href = '/assets/favicon_io/site.webmanifest'
