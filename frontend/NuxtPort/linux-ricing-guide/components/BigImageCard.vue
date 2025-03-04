23bc7724c3407dc0bc3449db21a46a74aeac9cca
Julian Mostbauer <julian.mostbauer@outlook.com>
2025-03-04 09:02:09 +0100

    added big image card component and started recreating the index page

---
+++ frontend/NuxtPort/linux-ricing-guide/components/BigImageCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="w-full mt-4 h-[calc(100%-1.5rem)]">
+    <div class="bg-white shadow-lg rounded-lg overflow-hidden">
+      <img :src="imagePath" class="w-full h-60 object-cover" alt="..." />
+      <div class="p-4 text-left">
+        <h5 class="text-xl font-bold">{{ title }}</h5>
+        <p class="text-gray-700 mt-2">
+          {{ description }}
+        </p>
+      </div>
+      <div class="p-4 text-center">
+        <a :href="link"
+          class="btn btn-primary w-full inline-block py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600">
+          <i class="fa-solid fa-link"></i> Visit
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  imagePath: string;
+  title: string;
+  description: string;
+  link: string;
+}>();
+</script>
--- frontend/NuxtPort/linux-ricing-guide/pages/index.vue
+++ frontend/NuxtPort/linux-ricing-guide/pages/index.vue
@@ -2,9 +2,41 @@
 </script>
 
 <template>
-  <div>
-    <div>
-      <h1 class="text-xl mt-8"> Hello from home</h1>
-    </div>
-</div>
+  <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
+    <BigImageCard imagePath="img/card-distributions.webp" title="Distributions"
+      description="List of some linux distributions you can use. These contain common but also some exotic ones."
+      link="distros.html"></BigImageCard>
+
+
+    <BigImageCard imagePath="img/card-package-managers.webp" title="Package Managers"
+      description="Selection of Package Managers you can use to install software on your linux machine."
+      link="package-managers.html"></BigImageCard>
+
+
+    <BigImageCard imagePath="img/card-setups.webp" title="Setups"
+      description="Showcase of our personal setups we use on our machines. Feel free to get inspired and copy some aspects."
+      link="personal-setups.html"></BigImageCard>
+
+
+    <BigImageCard imagePath="img/card-window-manager.webp" title="Window-Managers"
+      description="Controls the appearance, and behavior of application windows in a graphical interface."
+      link="window-managers.html"></BigImageCard>
+
+
+    <BigImageCard imagePath="img/card-terminals.webp" title="Terminals"
+      description="List of linux terminals you can install." link="terminals.html"></BigImageCard>
+
+
+    <BigImageCard imagePath="img/card-terminal-themes.webp" title="Terminal themes"
+      description="A small selection of themes for your terminal." link="terminal-themes.html"></BigImageCard>
+
+
+    <BigImageCard imagePath="img/card-shells.webp" title="Shells"
+      description="Some shells you can use on your linux machine." link="shells.html"></BigImageCard>
+
+
+    <BigImageCard imagePath="img/card-desktop-environments.webp" title="Desktop Environments"
+      description="Desktop Environments are the graphical interfaces that you see on your screen."
+      link="desktop-environments.html"></BigImageCard>
+  </div>
 </template>
