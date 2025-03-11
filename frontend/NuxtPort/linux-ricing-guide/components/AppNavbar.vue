dbebcb01aa255594fee6061bd81d235a04a2de45
Julian Mostbauer <julian.mostbauer@outlook.com>
2025-03-11 12:34:00 +0100

    changed navbar and history

--- frontend/NuxtPort/linux-ricing-guide/components/AppNavbar.vue
+++ frontend/NuxtPort/linux-ricing-guide/components/AppNavbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="navbar bg-base-200 border-b-2 border-gray-800 sticky top-0 z-50">
+  <div class="navbar bg-transparent bg-opacity-70 backdrop-blur-md border-b-2 border-gray-800 sticky top-0 z-50">
     <div class="flex-1">
       <div class="breadcrumbs text-xl">
         <ul class="ml-2">
