e0ad91e06ce156bcefde5249eb29f025d1f03221
Julian Mostbauer <julian.mostbauer@outlook.com>
2025-03-11 11:03:19 +0100

    added multiple low quality versions

--- frontend/NuxtPort/linux-ricing-guide/pages/index.vue
+++ frontend/NuxtPort/linux-ricing-guide/pages/index.vue
@@ -61,32 +61,32 @@
       </article>
     </div>
     <div class="m-8 grid grid-cols-1 md:grid-cols-4 gap-4">
-      <BigImageCard imagePath="img/card-distributions.webp" title="Distributions"
+      <BigImageCard imagePath="img/mini2/card-distributions.webp" title="Distributions"
         description="List of some linux distributions you can use. These contain common but also some exotic ones."
         link="distros"></BigImageCard>
 
-      <BigImageCard imagePath="img/mini/card-package-managers.webp" title="Package Managers"
+      <BigImageCard imagePath="img/mini2/card-package-managers.webp" title="Package Managers"
         description="Selection of Package Managers you can use to install software on your linux machine."
         link="package-managers"></BigImageCard>
 
-      <BigImageCard imagePath="img/mini/card-setups.webp" title="Setups"
+      <BigImageCard imagePath="img/mini2/card-setups.webp" title="Setups"
         description="Showcase of our personal setups we use on our machines. Feel free to get inspired and copy some aspects."
         link="our-setups"></BigImageCard>
 
-      <BigImageCard imagePath="img/mini/card-window-manager.webp" title="Window-Managers"
+      <BigImageCard imagePath="img/mini2/card-window-manager.webp" title="Window-Managers"
         description="Controls the appearance, and behavior of application windows in a graphical interface."
         link="window-managers"></BigImageCard>
 
-      <BigImageCard imagePath="img/card-terminals.webp" title="Terminals"
+      <BigImageCard imagePath="img/mini5/card-terminals.webp" title="Terminals"
         description="List of linux terminals you can install." link="terminals"></BigImageCard>
 
-      <BigImageCard imagePath="img/card-terminal-themes.webp" title="Terminal themes"
+      <BigImageCard imagePath="img/mini5/card-terminal-themes.webp" title="Terminal themes"
         description="A small selection of themes for your terminal." link="terminal-themes"></BigImageCard>
 
-      <BigImageCard imagePath="img/card-shells.webp" title="Shells"
+      <BigImageCard imagePath="img/mini5/card-shells.webp" title="Shells"
         description="Some shells you can use on your linux machine." link="shells"></BigImageCard>
 
-      <BigImageCard imagePath="img/card-desktop-environments.webp" title="Desktop Environments"
+      <BigImageCard imagePath="img/mini2/card-desktop-environments.webp" title="Desktop Environments"
         description="Desktop Environments are the graphical interfaces that you see on your screen."
         link="desktop-environments"></BigImageCard>
     </div>
