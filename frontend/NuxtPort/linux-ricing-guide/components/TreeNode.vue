861f13e6299c2bb29b0ff367d6ec107def72508c
Julian Mostbauer <julian.mostbauer@outlook.com>
2025-03-13 10:45:45 +0100

    refactor of icon size
    removed unnecessary code

--- frontend/NuxtPort/linux-ricing-guide/components/TreeNode.vue
+++ frontend/NuxtPort/linux-ricing-guide/components/TreeNode.vue
@@ -2,8 +2,8 @@
   <ul v-if="!isRoot || node.Children.length > 0">
     <li v-if="node.HasIndex">
       <a :href="node.Value?.path || '/'">
-        <Icon :name="'fa6-solid:' + (isRoot ? 'house' : 'file-lines')" size="16" />
-        {{ routeName(node.Value?.path, "Home") }}
+        <Icon :name="'fa6-solid:' + (isRoot ? 'house' : 'file-lines')" :size="iconSize" />
+        {{ routeName(node.Value?.path, isRoot ? "Home" : "Unknown Location") }}
       </a>
     </li>
 
@@ -11,14 +11,14 @@
     <li v-for="child in node.Children" :key="child.Value?.path">
       <details v-if="child.Children.length > 0" open>
         <summary>
-          <Icon name="fa6-solid:folder" size="16" />
+          <Icon name="fa6-solid:folder" :size="iconSize" />
           {{ routeName(child.Value?.path) }}
         </summary>
         <TreeNode :node="child" />
       </details>
 
       <a v-else :href="child.Value?.path || '/'">
-        <Icon name="fa6-solid:file-lines" size="16" />
+        <Icon name="fa6-solid:file-lines" :size="iconSize" />
         {{ routeName(child.Value?.path) }}
       </a>
     </li>
@@ -29,7 +29,9 @@
 
 <script lang="ts" setup>
 import type { PropType } from 'vue';
-import { Tree, Node, routeName } from 'assets/utils/routeTree'
+import { Node, routeName } from 'assets/utils/routeTree'
+
+const iconSize: number = 20;
 
 const props = defineProps({
   node: {
