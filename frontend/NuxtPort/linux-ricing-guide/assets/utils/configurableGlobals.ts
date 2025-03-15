e67c570e7393c75b1a5cb9dd29d68744287795fd
Julian Mostbauer <julian.mostbauer@outlook.com>
2025-03-15 22:38:16 +0100

    made wrapper component

--- frontend/NuxtPort/linux-ricing-guide/assets/utils/configurableGlobals.ts
+++ frontend/NuxtPort/linux-ricing-guide/assets/utils/configurableGlobals.ts
@@ -1,6 +1,7 @@
 const aivailableProviders = ['mdi', 'fa6-solid']
 
 let activeProvider = aivailableProviders[0]
-const iconToLink = (name: string) => activeProvider + ':' + name
+const iconToLink = (name: string, provider?: string) =>
+    provider ? provider : activeProvider + ':' + name
 
 export { iconToLink }
---
+++ frontend/NuxtPort/linux-ricing-guide/components/DynamicIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Icon :name="iconToLink(name, provider)" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { iconToLink } from '~/assets/utils/configurableGlobals';
+  defineProps<{
+    name: string
+    provider?: string
+  }>();
+</script>
