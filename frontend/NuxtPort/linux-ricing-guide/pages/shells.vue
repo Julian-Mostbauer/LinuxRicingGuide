eff9837e6e2461a8001a58079ebf62b3023f0538
Julian Mostbauer <julian.mostbauer@outlook.com>
2025-03-15 14:42:51 +0100

    ported shells page to new animated component

--- frontend/NuxtPort/linux-ricing-guide/pages/shells.vue
+++ frontend/NuxtPort/linux-ricing-guide/pages/shells.vue
@@ -1,18 +1,6 @@
 <template>
-  <div class="flex flex-col items-center">
-    <SmallImageCard title="Bash"
-      description="Bash is the default shell for most Linux distributions. You can customize Bash using Bash configuration files."
-      link="https://www.gnu.org/software/bash/" image-path="https://images.seeklogo.com/logo-png/39/1/bash-logo-png_seeklogo-393098.png">
-    </SmallImageCard>
-
-    <SmallImageCard title="Zsh"
-      description="Zsh is a powerful shell that is known for its extensibility. You can customize Zsh using Zsh configuration files."
-      link="https://ohmyz.sh/" image-path="https://ohmyz.sh/img/ohmyzsh-logo-ansi.png"></SmallImageCard>
-
-    <SmallImageCard title="Fish"
-      description="Fish is a user-friendly shell that is known for its simplicity. You can customize Fish using Fish configuration files."
-      link="https://fishshell.com/" image-path="https://fishshell.com/assets/img/Terminal_Logo2_CRT_Flat.png">
-    </SmallImageCard>
+  <div class="overflow-hidden">
+    <SmallImageCardList :cardContents="shells" />
   </div>
 </template>
 
@@ -20,4 +8,26 @@
 definePageMeta({
   icon: 'floppy-disk',
 });
+
+const shells = [
+  {
+    title: "Bash",
+    description: "Bash is the default shell for most Linux distributions. You can customize Bash using Bash configuration files.",
+    link: "https://www.gnu.org/software/bash/",
+    imagePath: "https://images.seeklogo.com/logo-png/39/1/bash-logo-png_seeklogo-393098.png"
+  },
+  {
+    title: "Zsh",
+    description: "Zsh is a powerful shell that is known for its extensibility. You can customize Zsh using Zsh configuration files.",
+    link: "https://ohmyz.sh/",
+    imagePath: "https://ohmyz.sh/img/ohmyzsh-logo-ansi.png"
+  },
+  {
+    title: "Fish",
+    description: "Fish is a user-friendly shell that is known for its simplicity. You can customize Fish using Fish configuration files.",
+    link: "https://fishshell.com/",
+    imagePath: "https://fishshell.com/assets/img/Terminal_Logo2_CRT_Flat.png"
+  }
+
+]
 </script>
