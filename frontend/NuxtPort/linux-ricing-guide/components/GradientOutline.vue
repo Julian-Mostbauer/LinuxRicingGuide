12f804af529db0de3f3af70f72de6ab4d541b542
Integr-0 <erik.reitbauer@outlook.de>
2025-03-18 08:30:39 +0100

    Fixed GradientOutline.vue having the wrong radius

--- frontend/NuxtPort/linux-ricing-guide/components/GradientOutline.vue
+++ frontend/NuxtPort/linux-ricing-guide/components/GradientOutline.vue
@@ -14,7 +14,7 @@ defineProps<{
   <div :class="`${className} card entry relative p-[0.2rem] bg-transparent h-full`" :style="{
     '--x': `${x - (el?.offsetLeft ?? 0)}px`,
     '--y': `${y - (el?.offsetTop ?? 0)}px`,
-    '--border-radius': 'var(--radius-box)',
+    '--border-radius': 'calc(var(--radius-box)*1.08)',
     '--circle-width': circleWidth,
   }" ref="el">
     <slot />
