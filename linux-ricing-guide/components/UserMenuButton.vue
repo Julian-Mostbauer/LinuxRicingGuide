b6d6060f4a1831631c784ba8482b8fff7eaf632a
Julian Mostbauer <julian.mostbauer@outlook.com>
2025-04-09 10:51:23 +0200

    restyled and moved profile icon into dropdown

--- linux-ricing-guide/components/UserMenuButton.vue
+++ linux-ricing-guide/components/UserMenuButton.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
-    <div class="dropdown dropdown-end rounded-full bg-base-200">
+    <div class="dropdown dropdown-end">
       <div tabindex="0" role="button" class="btn btn-circle m-0.5 overflow-clip">
-        <img :src="userIcon" alt="Theme Picker" class="w-12 h-12" />
+        <DynamicIcon :names="{ default: 'user' }" size="20" />
       </div>
       <ul tabindex="0" class="card card-body dropdown-content bg-base-300 rounded-box z-1 p-2 shadow-2xl w-52">
         <li>
-          <div class="justify-start m-1 m-b-0 p-1 text-center">
-            <DynamicIcon :names="{ default: 'user' }" />
-            <p>Logged in as {{ userName }}</p>
+          <div class="border-2 border-base-100 shadow-md rounded-full flex items-center gap-2 p-1 text-center">
+            <img :src="userIcon" alt="Theme Picker" class="w-12 h-12 rounded-full" />
+            <p class="text-lg font-bold text-center">{{ userName }}</p>
           </div>
         </li>
         <div class="divider m-0" />
