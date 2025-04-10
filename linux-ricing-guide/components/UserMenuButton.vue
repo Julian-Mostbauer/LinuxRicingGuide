90f134f07589baabb204b336d3ed12a6f567e23d
Integr-0 <erik.reitbauer@outlook.de>
2025-04-10 10:12:00 +0200

    Login support via Auth0

--- linux-ricing-guide/components/UserMenuButton.vue
+++ linux-ricing-guide/components/UserMenuButton.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <div class="dropdown dropdown-end">
+    <div v-if="auth0.isLoading.value">
+      <div class="skeleton h-10 w-10 shrink-0 rounded-full"/>
+    </div>
+    <div v-else class="dropdown dropdown-end">
       <div tabindex="0" role="button" class="btn btn-circle m-0.5 overflow-clip">
-        <DynamicIcon :names="{ default: 'user' }" size="20" />
+        <img :src="auth0.user.value?.picture" alt="Account" class="w-12 h-12 rounded-full" />
       </div>
       <ul tabindex="0" class="card card-body dropdown-content bg-base-300 rounded-box z-1 p-2 shadow-2xl w-52">
         <li>
           <div class="border-2 border-base-100 shadow-md rounded-full flex items-center gap-2 p-1 text-center">
-            <img :src="userIcon" alt="Theme Picker" class="w-12 h-12 rounded-full" />
-            <p class="text-lg font-bold text-center">{{ userName }}</p>
+            <img :src="auth0.user.value?.picture" alt="Account" class="w-12 h-12 rounded-full" />
+            <p class="text-lg font-bold text-center">{{ auth0.user.value?.name }}</p>
           </div>
         </li>
         <div class="divider m-0" />
@@ -19,7 +22,7 @@
           </div>
         </li>
         <li>
-          <div class="btn btn-error justify-start m-1 p-1 text-center w-full">
+          <div class="btn btn-error justify-start m-1 p-1 text-center w-full" @click="logout()">
             <DynamicIcon :names="{ default: 'cog' }" />
             <p>Log out</p>
           </div>
@@ -30,6 +33,10 @@
 </template>
 
 <script lang="ts" setup>
-var userName = ref('John Doe')
-var userIcon = ref('https://i2-prod.mirror.co.uk/article20427472.ece/ALTERNATES/s1200c/0_PAY-HUMAN-DOG.jpg')
+import {useAuth0} from "@auth0/auth0-vue";
+
+const auth0 = useAuth0();
+const logout = () => {
+  auth0.logout({ logoutParams: { returnTo: window.location.origin } });
+};
 </script>
