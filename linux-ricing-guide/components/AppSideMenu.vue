0d8dbbf2a0f9c7ad8b16a9c6e9b62fbb4d6f1763
Julian Mostbauer <julian.mostbauer@outlook.com>
2025-04-11 08:47:28 +0200

    styling consistency. gutter on both sides

--- linux-ricing-guide/components/AppSideMenu.vue
+++ linux-ricing-guide/components/AppSideMenu.vue
@@ -1,8 +1,7 @@
 <template>
-    <div
-        class="menu rounded-r-[1.5rem] mt-2 mb-2 bg-base-200 text-base-content min-h-[98%] w-80 border-2 border-l-0 border-base-300 overflow-hidden">
-        <TreeNode v-if="fileTree.Root" :node="fileTree.Root" :isRoot="true" />
-    </div>
+  <div class="menu rounded-box mt-2 mb-2 bg-base-200 text-base-content min-h-[98%] w-96 overflow-hidden">
+    <TreeNode v-if="fileTree.Root" :node="fileTree.Root" :isRoot="true" />
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -10,8 +9,7 @@ import { useRouter } from 'vue-router';
 import { ref } from 'vue';
 import { Tree } from 'assets/utils/routeTree'
 
-const router = useRouter();
-const routes = router.getRoutes();
+const routes = useRouter().getRoutes();
 
 const fileTree = ref(new Tree());
 routes.forEach(route => fileTree.value.add(route));
