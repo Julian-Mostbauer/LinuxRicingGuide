61d5ab64eff4b5fb0f5d15aeab5f38744172bac5
Julian Mostbauer <julian.mostbauer@outlook.com>
2025-04-18 14:03:02 +0200

    backup commit for rollbacks

--- linux-ricing-guide/components/AppSideMenu.vue
+++ linux-ricing-guide/components/AppSideMenu.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="menu rounded-r-box mt-2 mb-2 bg-base-200 text-base-content min-h-[98%] w-96 overflow-hidden">
     <div class="flex justify-end p-2">
-      <button @click="toggleNavMode" class="btn btn-primary">Switch Mode</button>
+      <button @click="toggleNavMode" class="btn btn-primary">
+        {{ navMode }}
+      </button>
     </div>
     <div v-if="navMode === 'drawer'">
       <DrawerNode v-if="fileTree.Root" :node="fileTree.Root" :isRoot="true" />
@@ -13,24 +15,24 @@
 </template>
 
 <script lang="ts" setup>
-import { useRouter } from 'vue-router';
-import { ref } from 'vue';
-import { TreeBuilder } from 'assets/utils/routeTree';
+import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+import { TreeBuilder } from 'assets/utils/routeTree'
 
-const fileTree = ref(TreeBuilder.FromRoutes(useRouter().getRoutes()));
+const fileTree = ref(TreeBuilder.FromRoutes(useRouter().getRoutes()))
 
-const modes = ['drawer', 'graph'];
-const navMode = ref(modes[0]);
-const currentModeIdx = ref(0);
+const modes = ['drawer', 'graph']
+const navMode = ref(modes[0])
+const currentModeIdx = ref(0)
 
 if (typeof window !== 'undefined') {
-  navMode.value = localStorage.getItem('navMode') || modes[0];
-  currentModeIdx.value = modes.indexOf(navMode.value);
+  navMode.value = localStorage.getItem('navMode') || modes[0]
+  currentModeIdx.value = modes.indexOf(navMode.value)
 }
 
 const toggleNavMode = () => {
-  navMode.value = modes[(currentModeIdx.value + 1) % modes.length];
-  currentModeIdx.value = (currentModeIdx.value + 1) % modes.length;
-  localStorage.setItem('navMode', navMode.value);
-};
+  navMode.value = modes[(currentModeIdx.value + 1) % modes.length]
+  currentModeIdx.value = (currentModeIdx.value + 1) % modes.length
+  localStorage.setItem('navMode', navMode.value)
+}
 </script>
