a28df00b5a5c7876711e6cfa9acbeee95bb1fda3
Julian Mostbauer <julian.mostbauer@outlook.com>
2025-05-06 15:02:11 +0200

    added endpoints for up/downvoting distros

---
+++ linux-ricing-guide/server/api/dbWrapper/distros/downvote.ts
@@ -0,0 +1,20 @@
+export default defineEventHandler(async (event) => {
+  const endPoint: string = useRuntimeConfig().public.backendAddress as string
+
+  const { name, id } = await readBody(event);
+
+  if (!id) {
+    return { error: 'ID is required' }
+  }
+
+  try {
+    const response = await $fetch(`${endPoint}/distros/${name}/downvote`, { timeout: 5000,method:'POST', headers: { 'X-User-ID': id} })
+    return { data: response }
+  }catch (error: any) {
+    console.error('Error fetching distro info:', error)
+    if (error.name === 'FetchError' && error.type === 'request-timeout') {
+      return { error: 'Request timed out' }
+    }
+    return { error: error.message || 'Unknown error' }
+  }
+});
