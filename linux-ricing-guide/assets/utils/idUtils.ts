ed67f2f8f20a81bc80fe474d0aefe097b8064cd4
Julian Mostbauer <julian.mostbauer@outlook.com>
2025-05-09 11:24:35 +0200

    no more logging

--- linux-ricing-guide/assets/utils/idUtils.ts
+++ linux-ricing-guide/assets/utils/idUtils.ts
@@ -1,19 +1,17 @@
-import type { Auth0VueClient } from "@auth0/auth0-vue";
+import type { Auth0VueClient } from '@auth0/auth0-vue'
 
 const getUserID = async (auth0: Auth0VueClient): Promise<string> => {
-  let counter = 2;
-  while(auth0.user.value?.sub == undefined){
-    await new Promise(resolve => setTimeout(resolve, counter));
-    counter *= 2;
+  let counter = 2
+  while (auth0.user.value?.sub == undefined) {
+    await new Promise((resolve) => setTimeout(resolve, counter))
+    counter *= 2
 
-    if(counter > 10000){
-      console.error("Timeout waiting for auth0 to load");
-      return "";
+    if (counter > 10000) {
+      console.error('Timeout waiting for auth0 to load')
+      return ''
     }
   }
-  const sub = auth0.user.value?.sub
-  console.log(sub)
-  return sub ?? "";
+  return auth0.user.value?.sub ?? ''
 }
 
-export { getUserID}
+export { getUserID }
