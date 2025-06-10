fb7a9a54dd1733e02393d946238701e975c3e29f
Integr-0 <erik.reitbauer@outlook.de>
2025-06-10 11:34:33 +0200

    Added about page to show what this site is about.

--- linux-ricing-guide/pages/about-this-page.vue
+++ linux-ricing-guide/pages/about-this-page.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
-
+definePageMeta({
+    icons: {
+        default: 'pager',
+    }
+});
 </script>
 
 <template>
-
+    <div class="bg-base-300 p-20">
+        <div class="text-left">
+            <div class="max-w-md">
+                <h1 class="text-5xl font-bold">About This Site</h1>
+                <p class="py-6">
+                    This site is a collection of resources and guides for customizing and personalizing your Linux desktop environment, commonly referred to as "ricing". It aims to provide users with the tools and knowledge needed to create a unique and visually appealing desktop experience.
+                </p>
+                <p class="py-6">
+                    The site includes guides on various topics such as desktop environments, themes, icons, widgets, and more.
+                </p>
+                <p class="py-6">
+                    Whether you're a beginner looking to get started with Linux customization or an experienced user seeking new ideas, this site has something for everyone. We hope you find the resources helpful and inspiring!
+                </p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
