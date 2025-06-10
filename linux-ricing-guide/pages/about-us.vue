f567ea6a7e74a02f2d494766eb5386e153addc3a
Integr-0 <erik.reitbauer@outlook.de>
2025-06-10 11:45:22 +0200

    Updated and fixed the about us page

--- linux-ricing-guide/pages/about-us.vue
+++ linux-ricing-guide/pages/about-us.vue
@@ -10,13 +10,13 @@ definePageMeta({
         <div class="text-left">
             <div class="max-w-md">
                 <h1 class="text-5xl font-bold">About Us</h1>
-                <p class="py-6">
+                <p class="pt-6">
                     This page was created by Julian Mostbauer, Erik Reitbauer, Arthur Dirnberger and Koorosh Hassani.
                 </p>
-                <p class="py-6">
+                <p class="pt-6">
                     We are a group of Linux enthusiasts who love to customize and personalize our desktop environments. Our goal is to share our knowledge and experiences with the community, helping others to create their own unique Linux setups.
                 </p>
-                <p class="py-6">
+                <p class="pt-6">
                     This guide is a collection of tips, tricks, and resources that we have found useful in our own ricing journeys. We hope that it will inspire you to explore the world of Linux customization and find your own style.
                 </p>
 
